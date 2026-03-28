3c7bbb600d375016e1ec6f82dc58d69c07fab20e
matheus-arj <matheus.arj@hotmail.com>
2026-03-27 21:06:51 -0300

    feat: created post upload api to upload file in database, also created upload controller file with call service function to insert file

---
+++ laravel/routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\InstrumentController;
+use App\Http\Controllers\UploadController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/upload', [UploadController::class, 'store']);
